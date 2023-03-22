df4838abecce1cb8d9ac689245b1fcd05075b906
AnaK2005 <anacarolina.v.santos123@gmail.com>
2023-03-21 22:45:53 -0300

    model Filme

--- app/Http/Controllers/cadastroFilme.php
+++ app/Http/Controllers/cadastroFilme.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\cadastroFilmeModel;
 
 class cadastroFilme extends Controller
 {
-    public function buscaCadastroFilme(){
+    public function buscaCadastroFilme(Request $request){
 
         return View('cadastroFilme');
+    }
+
+    public function cadastrarFilme() {
+        $dadosFilme = $request->validate([
+            'filme' => 'string|required',
+            'atores'=> 'string|required',
+            'dataLancamento' => 'string|required',
+            'sinopse' => 'string|required',
+            'capa' => 'string|required'
+        ]);
+
+        cadastroFilmeModel::create($dadosFilme); 
+
+        return Redirect::route('/home');
+
+        
+
     }
 }
