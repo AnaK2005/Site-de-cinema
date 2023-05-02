5c0e7447029f528d64b28ea15c603a4427acfc09
AnaK2005 <119542858+AnaK2005@users.noreply.github.com>
2023-05-02 10:05:15 -0300

    atualizacao alterar

--- resources/views/gerenciadorFuncionario.blade.php
+++ resources/views/gerenciadorFuncionario.blade.php
@@ -32,8 +32,14 @@
       <th scope="row">{{$dadosfuncionarios->id}}</th>
       <td>{{$dadosfuncionarios->nomefun}}</td>
       <td>{{$dadosfuncionarios->emailfun}}</td>
-      <td>X</td>
-      <td>X</td>  
+      <td><a href="{{route('mostrar-funcionario',$dadosfuncionarios->id)}}">Alterar</a></td>
+      <td>
+        <form method="post" action="{{route('apagar-funcionario',$dadosfuncionarios->id)}}">
+            @method('delete')
+            @csrf 
+            <button type="submit" class="btn-danger">Excluir</button>
+        </form>
+      </td>  
 
     </tr>
    @endforeach
